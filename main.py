341bb69549ae3d64fde6a8365120b2dc07e7a436
at900 <ashleytackett999@gmail.com>
2026-09-20 20:24:15 -0500

    added http server functionality and RSA key generation

---
+++ main.py
@@ -0,0 +1,94 @@
+import json
+import time
+import uuid
+
+from fastapi import FastAPI
+from jwcrypto import jwk
+
+# dict to store keys along with their expiration timestamp
+KEYS = {}
+
+app = FastAPI()
+
+def generate_key():
+
+    kid = str(uuid.uuid4())
+    
+    key = jwk.JWK.generate(
+        kty='RSA', 
+        size=2048, 
+        alg='RS256', 
+        use='sig', 
+        kid=kid
+    )
+
+    expires_in = 1 * 60 * 60 # expires in 1 hour
+    expiration_timestamp = int(time.time()) + expires_in
+
+    KEYS[kid] = {
+        "key_obj": key,
+        "expires_at": expiration_timestamp,
+        "kid": kid
+    }
+    return kid
+
+@app.on_event("startup")
+def startup():
+    generate_key()
+    generate_key()
+    generate_key()
+    # print(generate_key())
+
+@app.post("/auth")
+def retrn_key():
+    kid = generate_key()
+    return KEYS[kid].key_obj
+
+@app.get("/.well-known/jwks.json")
+def return_json():
+    now = int(time.time())
+    valid_keys = []
+    for key in KEYS.values():
+        if key["expires_at"] > now:
+            valid_keys.append(key["key_obj"].export_public(as_dict=True))
+    return {"keys": valid_keys}
+
+
+
+
+
+
+
+
+
+
+
+
+    # public_jwk = key.export_public(as_dict=True)
+
+    # # 3. Create a public JSON Web Key Set (JWKS) structure
+    # public_jwks = {
+    #     "keys": [public_jwk]
+    # }
+
+    # # 4. Export the Private JWK (Keep this secret!)
+    # private_jwk = key.export_private(as_dict=True)
+
+    # # Print the Public JWKS
+    # # print("Public JWKS:")
+    # # print(json.dumps(public_jwks, indent=2))
+
+    # print(public_jwk)
+
+# generate_key()
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello, FastAPI!"}
+
+
+# @app.get("/.well-known")
+# def read_root():
+#     return {"message": "Hello, FastAPI!"}
